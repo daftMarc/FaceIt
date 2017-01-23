38e564d1ef5b333ac520615f9ef040c17da40b97
daftMarc <marc.familari@gmail.com>
2017-01-23 18:44:37 +0100

    end of 1st lesson

---
+++ FaceIt/FaceViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Marc FAMILARI on 1/23/17.
+//  Copyright © 2017 Marc FAMILARI. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+
+}
+
